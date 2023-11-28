56216f8da0f281c1381947516195ac78770a3bce
minayemsv <93090255+minayemsv@users.noreply.github.com>
2023-11-28 18:49:20 +0400

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -12,34 +12,34 @@ namespace TDD_SQA
         {
 
         }
-        public int addinteger(int integer1, int integer2)
+        public int addinginteger(int integer1, int integer2)
         {
             int sum = integer1 + integer2;
             return sum;
         }
-        public float addfloat(float float1, float float2)
+        public float addingfloat(float float1, float float2)
         {
             float sum = float1 + float2;
             return sum;
         }
-        public double adddouble(double double1, double double2)
+        public double addingdouble(double double1, double double2)
         {
             double sum = double1 + double2;
             return sum;
         }
 
 
-        public int subtractinteger(int integer1, int integer2)
+        public int subtractinginteger(int integer1, int integer2)
         {
             int result = integer1 - integer2;
             return result;
         }
-        public float subtractfloat(float float1, float float2)
+        public float subtractingfloat(float float1, float float2)
         {
             float result = float1 - float2;
             return result;
         }
-        public double subtractdouble(double double1, double double2)
+        public double subtractingdouble(double double1, double double2)
         {
             double result = double1 - double2;
             return result;
@@ -47,17 +47,17 @@ namespace TDD_SQA
 
 
 
-        public int multiplyinteger(int integer1, int integer2)
+        public int multiplyinginteger(int integer1, int integer2)
         {
             int result = integer1 * integer2;
             return result;
         }
-        public float multiplyfloat(float float1, float float2)
+        public float multiplyingfloat(float float1, float float2)
         {
             float result = float1 * float2;
             return result;
         }
-        public double multiplydouble(double double1, double double2)
+        public double multiplyingdouble(double double1, double double2)
         {
             double result = double1 * double2;
             return result;
@@ -65,17 +65,17 @@ namespace TDD_SQA
 
 
 
-        public int divisioninteger(int integer1, int integer2)
+        public int divisioninginteger(int integer1, int integer2)
         {
             int result = integer1 / integer2;
             return result;
         }
-        public float divisionfloat(float float1, float float2)
+        public float divisioningfloat(float float1, float float2)
         {
             float result = float1 / float2;
             return result;
         }
-        public double divisiondouble(double double1, double double2)
+        public double divisioningdouble(double double1, double double2)
         {
             double result = double1 / double2;
             return result;
